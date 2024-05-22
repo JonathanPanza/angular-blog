cd6344c22a6acc982569d88a84f681fa403b0467
CupDz <jonathandev192@gmail.com>
2024-05-22 13:24:03 -0300

    components createds with styles

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.css',
+  standalone: true
+})
+export class MenuBarComponent { }
